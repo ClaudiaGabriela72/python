4bf79f9194772c26c41a89c069cf316266f65e51
ClaudiaGabriela72 <71900311+ClaudiaGabriela72@users.noreply.github.com>
2024-11-19 09:33:22 -0400

    at

--- setup.py/setup.py
+++ setup.py/setup.py
@@ -2,22 +2,25 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="projC",                      # Nome do projeto
-    version="0.1.0",                         # Versão do projeto
-    author="Claudia Gabriela",                       # Autor
-    author_email="claudiagabriela578@gmail.com",     # Email do autor
+    name="projC",                     
+    version="0.1.0",                        
+    author="Claudia Gabriela",                       
+    author_email="claudiagabriela578@gmail.com",     
     description="Ver clima",
     long_description_content_type="text/markdown",
-    url="https://claudiagabriela72.github.io/python/",  # URL do projeto, ex.: GitHub
-    packages=find_packages(),                # Encontra pacotes automaticamente
-    install_requires=[                       # Dependências do projeto
-        "requests",                          # Exemplo de dependência
+    url="https://claudiagabriela72.github.io/python/",  
+    packages=find_packages(),                
+    install_requires=[                       
+        "requests>=2.0.0",                         
         "tkinter",
+        "PIL",
+        "pytz",
+        "json"
     ],
-    classifiers=[                            # Classificações do PyPI
+    classifiers=[                            
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',                 # Versão mínima do Python
+    python_requires='>=3.6',                 
 )
